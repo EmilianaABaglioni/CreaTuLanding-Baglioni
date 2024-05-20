2fd89252eea8e58970b3c9c68cf8c520bdd7f810
EmilianaABaglioni <emi.baglioni@outlook.com>
2024-05-20 20:09:52 -0300

    Proyecto React CH - Mithra - eCommerce

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ItemListContainer = ({ title }) => {
+  return (
+    <>
+      <div>
+        <p>{title}</p>
+      </div>
+    </>
+  )
+}
+
+export default ItemListContainer
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CartWidget from '../CartWidget/CartWidget'
+import styles from './Styles/NavBar.module.scss'
+
+const NavBar = () => {
+  return (
+    <>
+      <CartWidget />
+    </>
+  )
+}
+
+export default NavBar
+
