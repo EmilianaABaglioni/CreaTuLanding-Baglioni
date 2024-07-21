be1461e0b4e4e8f8488b48d5db61c861e8781a23
EmilianaABaglioni <emi.baglioni@outlook.com>
2024-07-21 19:59:30 -0300

    Avances Cart Context

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,15 +5,29 @@ const Context = createContext();
 export const CartContextProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
+  const inCart = (id) => {
+    return cart.some((product) => product.id === id)
+  }
+
   const addItem = (productToAdd, quantity) => {
     const itemNew = {
       ...productToAdd,
       quantity
     };
 
-    setCart([itemNew]);
+    if(inCart(itemNew.id)){
+      const updateCart = cart.map((product) => {
+        if(product.id === itemNew.id){
+          return { ...product, quantity: product.quantity + itemNew.quantity}
+        }
+        return product
+      })
+      setCart(updateCart)
+    }else{
+      setCart([...cart, itemNew]);
+    }
   };
-
+   
   return (
     <Context.Provider value={{ cart, setCart, addItem }}>
       {children}
